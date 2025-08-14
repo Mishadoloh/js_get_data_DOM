2230aa6081ce80e998df8ed8135aeec20a83e859
Mykhailo Doloh <dologmihajlo31@gmail.com>
2025-08-14 21:44:55 +0300

    add fixed

--- src/scripts/totalPopulation.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-unused-vars */
-'use strict';
-const { populations } = require("./main");
-
-export const totalPopulation = populations.reduce((acc, curr) => acc + curr, 0);

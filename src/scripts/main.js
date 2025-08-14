1d51beff02e7e7a03c6906e030555b0220a16ca4
Mykhailo Doloh <dologmihajlo31@gmail.com>
2025-08-14 20:40:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+
+// eslint-disable-next-line max-len
+const population = Array.from(populationElements).map((el) =>
+  Number(el.textContent.replace(/,/g, '').trim()));
+
+const totalPopulation = population.reduce((sum, num) => sum + num, 0);
+
+export const averagePopulation = totalPopulation / population.length;
+
+export function formatNumber(num) {
+  return num.toLocaleString('en-US');
+}
+
+// eslint-disable-next-line max-len
+document.querySelector('.total-population').textContent =
+  formatNumber(totalPopulation);
